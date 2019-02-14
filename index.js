eb57181244da9535d49c5731fa740c93e0ff028a
Stephen Boniface <QC5795@engie.com>
2019-02-14 14:08:04 -0600

    adds AddFriend mutation and resolver

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-import express from 'express';
-import graphqlHTTP from 'express-graphql';
-import schema from './schema';
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('GraphQL is amazing!');
+app.get("/", (req, res) => {
+  res.send("GraphQL is amazing!");
 });
 
+class Friend {
+  constructor(id, { firstName, lastName, gender, language, email }) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.language = language;
+    this.email = email;
+  }
+}
+
+const friendDatabase = {};
+
 const root = {
-    friend: () => {
-        return {
-            "id": 12345,
-            "firstName": "Steve",
-            "lastName": "Boniface",
-            "emails": [{ email: "stevieray8450@gmail.com" }, { email: "myOtherEmail@email.com" }],
-            "language": "English",
-            "gender": "Male"
-        }
-    }
+  friend: () => {
+    return {
+      id: 12345,
+      firstName: "Steve",
+      lastName: "Boniface",
+      emails: [
+        { email: "stevieray8450@gmail.com" },
+        { email: "myOtherEmail@email.com" }
+      ],
+      language: "English",
+      gender: "Male"
+    };
+  },
+  createFriend: ({input}) => {
+      let id = require('crypto').randomBytes(10).toString('hex');
+      friendDatabase[id] = input;
+      return new Friend(id, input);
+  }
 }; // resolver
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true
-}));
+  })
+);
 
-app.listen(8080, () => console.log('running server on port 8080/graphql'));
+app.listen(8080, () => console.log("running server on port 8080/graphql"));
--- schema.js
+++ schema.js
@@ -1,21 +1,30 @@
 import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
-    type Email {
-        email: String
-    }
-
     type Friend {
         id: ID
         firstName: String
         lastName: String
         gender: String
         language: String
-        emails: [Email]!
+        email: String
     }
 
     type Query {
         friend: Friend
+    }
+
+    input FriendInput {
+        id: ID
+        firstName: String!
+        lastName: String
+        gender: String
+        language: String
+        email: String
+    }
+
+    type Mutation {
+        createFriend(input: FriendInput): Friend
     }
 `);
 
